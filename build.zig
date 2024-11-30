13b57476715dcc30b525777f45b043e20cbb2002
Jan Beelte <jan.beelte@mailbox.org>
2024-11-30 18:58:04 +0000

    (chore) removed code duplication from sample.

--- build.zig
+++ build.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
-
+const zig_idf = @import("zig_idf");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{
-        .whitelist = espressif_targets,
-        .default_target = espressif_targets[0],
+        .whitelist = zig_idf.espressif_targets,
+        .default_target = zig_idf.espressif_targets[0],
     });
     const optimize = b.standardOptimizeOption(.{});
 
@@ -25,67 +25,4 @@ pub fn build(b: *std.Build) !void {
     lib.linkLibC(); // stubs for libc
 
     b.installArtifact(lib);
-}
-
-// Targets config
-pub const espressif_targets: []const std.Target.Query = if (isEspXtensa())
-    xtensa_targets ++ riscv_targets
-else
-    riscv_targets;
-
-const riscv_targets = &[_]std.Target.Query{
-    // esp32-c3/c2
-    .{
-        .cpu_arch = .riscv32,
-        .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic_rv32 },
-        .os_tag = .freestanding,
-        .abi = .none,
-        .cpu_features_add = std.Target.riscv.featureSet(&.{ .m, .c, .zifencei, .zicsr }),
-    },
-    // esp32-c6/61/h2
-    .{
-        .cpu_arch = .riscv32,
-        .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic_rv32 },
-        .os_tag = .freestanding,
-        .abi = .none,
-        .cpu_features_add = std.Target.riscv.featureSet(&.{ .m, .a, .c, .zifencei, .zicsr }),
-    },
-    // esp32-p4 have .xesppie cpu-feature (espressif vendor extension)
-    .{
-        .cpu_arch = .riscv32,
-        .cpu_model = .{ .explicit = &std.Target.riscv.cpu.esp32p4 },
-        .os_tag = .freestanding,
-        .abi = .eabihf,
-        .cpu_features_sub = std.Target.riscv.featureSet(&.{ .zca, .zcb, .zcmt, .zcmp }),
-    },
-};
-const xtensa_targets = &[_]std.Target.Query{
-    // need zig-fork (using espressif-llvm backend) to support this
-    .{
-        .cpu_arch = .xtensa,
-        .cpu_model = .{ .explicit = &std.Target.xtensa.cpu.esp32 },
-        .os_tag = .freestanding,
-        .abi = .none,
-    },
-    .{
-        .cpu_arch = .xtensa,
-        .cpu_model = .{ .explicit = &std.Target.xtensa.cpu.esp32s2 },
-        .os_tag = .freestanding,
-        .abi = .none,
-    },
-    .{
-        .cpu_arch = .xtensa,
-        .cpu_model = .{ .explicit = &std.Target.xtensa.cpu.esp32s3 },
-        .os_tag = .freestanding,
-        .abi = .none,
-    },
-};
-
-fn isEspXtensa() bool {
-    var result = false;
-    for (std.Target.Cpu.Arch.xtensa.allCpuModels()) |model| {
-        result = std.mem.startsWith(u8, model.name, "esp");
-        if (result) break;
-    }
-    return result;
 }
